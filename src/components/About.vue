59cdecbdece3b95d28dc678db37b8ec89fe69561
Abdurrazerq-Mujerheed <mujerheed20@gmail.com>
2021-12-22 00:36:32 +0100

    Getting started

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>About Young Democrat Development</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Register and be a member</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  
+</style>
